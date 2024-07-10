65ed75b1009e7adda0f44584d80251ac7ba5fde7
SupunWellappili <supunchandana1814@gmail.com>
2024-07-11 00:57:08 +0530

    App Screen Created

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,12 @@
 import { View, Text } from 'react-native'
+import React from 'react'
 
 const index = () => {
   return (
     <View>
-      <Text>index</Text>
+      <Text className="text-5xl font-bold text-green-700">index</Text>
     </View>
-  )
-}
+  );
+};
 
-export default index
+export default index;
